eba6e136f32ebdf4629e81b30c4a7b1baab2bfc2
Ishan777 <ishanturku@gmail.com>
2016-12-05 15:02:24 +0200

    app file modififcation

--- ktd-mobile/src/app/app.component.ts
+++ ktd-mobile/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
+export class Killthedj {
   rootPage = HomePage;
 
   constructor(platform: Platform) {
--- ktd-mobile/src/app/app.module.ts
+++ ktd-mobile/src/app/app.module.ts
@@ -1,21 +1,31 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
+import { KTDAppRootComponent } from './ktd-app-root/ktd-app-root.component';
+import { KTDSessionPageComponent } from './ktd-app-root/ktd-session-page.component';
+import { KTDTracklistPageComponent } from './ktd-app-root/ktd-tracklist-page.component';
+
+
+const appRoutes: Routes = [
+  { path: 'session', component: KTDSessionPageComponent },
+  { path: 'session/:id', component: KTDTracklistPageComponent },
+  { path: '', component: KTDSessionPageComponent },
+  { path: '**', component: KTDSessionPageComponent }
+];
+
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    KTDAppRootComponent,
+    KTDSessionPageComponent,
+    KTDTracklistPageComponent
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { useHash: true })
   ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  bootstrap: [KTDAppRootComponent],
+  providers: []
 })
 export class AppModule {}
