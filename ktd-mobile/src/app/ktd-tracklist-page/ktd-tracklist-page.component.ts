29924d69b88ff100381bd816609d7cb7c619edf4
Ishan777 <ishanturku@gmail.com>
2016-12-05 15:05:34 +0200

    app again

---
+++ ktd-mobile/src/app/ktd-tracklist-page/ktd-tracklist-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+
+  selector: 'ktd-tracklist-page',
+  templateUrl: 'ktd-tracklist-page.component.html'
+})
+export class KTDTracklistPageComponent {
+
+}
