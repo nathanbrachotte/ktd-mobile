14995e92a08d17dcde5b3a6b5bec66894fcd6d30
nathanbrachotte <Tillou77>
2016-12-05 20:54:47 +0200

    try adding a file

---
+++ ktd-mobile/src/app/ktd-app-root/ktd-app-root.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+
+  selector: 'ktd-app-root',
+  templateUrl: 'ktd-app-root.component.html'
+})
+export class KTDAppRootComponent {
+  title: string = 'KTD app with only angular and no ionic';
+}
