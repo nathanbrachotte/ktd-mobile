6fcfbd9fce208a60f78f7ae754275771393e3452
nathanbrachotte <Tillou77>
2017-01-10 16:47:20 +0200

    tracklist and vote requests

--- ktd-mobile/src/pages/home/home.ts
+++ ktd-mobile/src/pages/home/home.ts
@@ -21,13 +21,61 @@ export class HomePage {
   menuPage = MenuPage;
   posts: any;
   post: string;
+  answer: any;
+  link : string;
+  data : any;
 
   constructor(public navCtrl: NavController, public http: Http) {
     this.http.get('http://localhost:6680/killthedj/session').map(res => res.json()).subscribe(data => {
       this.posts = data.active;
       console.log(this.posts);
     });
+    this.submit();
+    this.upvote();
+
+
   }
+
+
+  submit() {
+
+    this.link = 'http://localhost:6680/killthedj/searches';
+    //this.data = JSON.stringify({username: this.data.username});
+    this.data = JSON.stringify({
+      "query": {
+        "track_name": [
+          "Une ba"
+        ]
+      }
+    });
+
+    this.http.post(this.link, this.data).map(res => res.json()).subscribe(data => {
+        this.answer = data[0].tracks;
+      }, error => {
+        console.log("Oooops!");
+      });
+  }
+
+
+  upvote() {
+
+    this.link = 'http://localhost:6680/killthedj/searches';
+    //this.data = JSON.stringify({username: this.data.username});
+    this.data = JSON.stringify({
+      "query": {
+        "track_name": [
+          "Un"
+        ]
+      }
+    });
+
+    this.http.post(this.link, this.data).map(res => res.json()).subscribe(data => {
+      this.answer = data[0].tracks;
+    }, error => {
+      console.log("Oooops!");
+    });
+  }
+
 
   goUsers() {
     this.navCtrl.pop(UsersPage);
